b83a77025209e14664ae61dff225210778fa94eb
hasINI-123-006 <hasini2006.16@gmail.com>
2026-09-12 23:44:09 +0530

    Updated HealthController

---
+++ ep-backend/src/main/java/com/hasini/ai_interview_analyzer/controller/HealthController.java
@@ -0,0 +1,12 @@
+package com.hasini.ai_interview_analyzer.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "OK";
+    }
+}
